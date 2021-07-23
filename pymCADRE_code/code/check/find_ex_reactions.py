e371dcbe5b1e92c9eec9b7ae2267f255d8897eb6
Nantia Leonidou <nantia.leonidou@uni-tuebingen.de>
2021-07-23 16:09:12 +0200

    Update find_ex_reactions.py

--- pymCADRE_code/code/check/find_ex_reactions.py
+++ pymCADRE_code/code/check/find_ex_reactions.py
@@ -1,14 +1,7 @@
 __author__ = "Nantia Leonidou"
 __description__ = " Identification of all exchange reactions in a given Cobra model"
 
-########
-# NOTE:
-# Using the cobrapy functions to obtain the exchange, sink and demand reactions (model.sink, model.exchanges and model.demand), two reactions
-# (DM_T_antigen(g) and sink_citr-L) of the mouse model could not be identified as exchange reactions.
-# However, using the humanModel.mat, all reactions have been detected correctly.
-# --> Thus, the identification of all exchanges reactions is done manually, using the length of the reactions' metabolites
-#     as criterion, to ensure always the correct result.
-########
+
 from cobra import *
 
 
@@ -31,6 +24,3 @@ def find_ex_rxns(model):
             ex_rxns_ids.append(react.id)
 
     return ex_rxns_ids
-
-#model = io.mat.load_matlab_model('../../humanModel.mat')
-#print(find_ex_rxns(model))
