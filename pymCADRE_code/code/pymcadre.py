664274a1fd0b323591a123c58d2895fc7de72fc8
Nantia Leonidou <nantia.leonidou@uni-tuebingen.de>
2021-07-26 17:53:22 +0200

    add main

--- pymCADRE_code/code/pymcadre.py
+++ pymCADRE_code/code/pymcadre.py
@@ -4,31 +4,35 @@ __description__ = " Run pymCADRE "
 from time import process_time
 from rank.rank_reactions import *
 from prune.prune_model import *
-from test_Inputs.test_Inputs import *
 import pandas as pd
 import numpy as np
 from cobra import *
 from cobra.io.sbml import *
 
 
-# model
-model = io.read_sbml_model('../pre_processing/dataset/recon1_with_BOF_and_VBOF.xml')
+# read model
+model = io.read_sbml_model('../test_dataset/recon1_with_BOF_and_VBOF.xml')
 # genes
-G = pd.read_csv('../pre_processing/dataset/1_GPL570_GSE3397/1_GPL570_GSE3397_entrez_ids.csv')
+G = pd.read_csv('../test_dataset/1_GPL570_GSE3397/1_GPL570_GSE3397_entrez_ids.csv')
 G = list(G['ENTREZ_GENE_ID'])
 # ubiquity scores
-U = pd.read_csv('../pre_processing/dataset/1_GPL570_GSE3397/1_GPL570_GSE3397_ubiquity.csv', header=None)
+U = pd.read_csv('../test_dataset/1_GPL570_GSE3397/1_GPL570_GSE3397_ubiquity.csv', header=None)
 U = U.rename(columns={0: "Scores"})
 U = list(U['Scores'])
-# confidence scores
+#confidence_scores
 confidence_scores = pd.read_csv('../pre_processing/dataset/Recon1_confidence_scores_with_BOF_and_VBOF.csv')
 confidence_scores = np.float64(list(confidence_scores['Confidence Score']))
-# list with precursor metabolites
-precursorMets = '../pre_processing/dataset/key_metabolites_RECON1.txt'
 
 ##############################################
 # Generate order for reaction removal
 ##############################################
+# Gene ubiquity scores are converted to reaction expression evidence to
+# define the core (C) and non-core (NC) reaction sets. Inactive reactions
+# are identified and removed from the global model to produce the generic
+# model (GM) for subsequent pruning. Non-core reactions are ordered first
+# by expression and then by connectivity evidence to give the list P. Any
+# reactions with zero expression (i.e., associated, but non-expressed
+# genes) are also listed in the vector Z.
 
 print('Processing inputs and ranking reactions...')
 GM, C, NC, P, Z, model_C = rank_reactions(model, G, U, confidence_scores, [], method=1)
@@ -49,7 +53,7 @@ if genericStatus:
     ###############################################################
     print('Pruning reactions...')
     t0 = process_time()
-    PM, cRes = prune_model(GM, P, C, Z, eta, precursorMets, salvage_check=1, method=1)
+    PM, cRes = prune_model(GM, P, C, Z, eta, '../test_dataset/key_metabolites_RECON1.txt', salvage_check=1, method=1)
     # Stop the stopwatch / counter
     t_stop = process_time()
     # compute elapsed time
