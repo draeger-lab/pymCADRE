a6a70bebd4858a0ad10bdbc017fde32a3a6a78f8
Nantia Leonidou <nantia.leonidou@uni-tuebingen.de>
2023-04-07 02:58:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='pymCADRE',  
+    name='pymCADRE',
+    version='1.2.1'
     description='The pymCADRE tool is an advanced re-implementation of the metabolic Context-specificity Assessed by Deterministic Reaction Evaluation (mCADRE) algorithm in Python. It constructs tissue-specific metabolic models by leveraging gene expression data and literature-based evidence, along with network topology information.',
     long_description=long_description,
     long_description_content_type="text/markdown",
